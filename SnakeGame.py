9633862664c31aa7556bc17f403637f956b88063
Robin1331 <robin.bilo13@gmail.com>
2024-09-10 21:39:24 -0400

    Set the screen to around the middle of the screen

--- SnakeGame.py
+++ SnakeGame.py
@@ -39,6 +39,25 @@ window.resizable(False, False)
 score = 0
 direction = "right"
 
-lable = Label(window, text="Score{}".format(score), font=("Arial", 15))
+label = Label(window, text="Score:{}".format(score), font=("Arial", 20))
+label.pack()
+
+canvas = Canvas(window, bg=BACKROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH )
+canvas.pack()
+
+window.update()
+
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+x = int((screen_width/2) - (window_width/2))
+y = int((screen_height/2) - (window_height/2))
+
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+snake = Snake()
+food = Food()
 
 window.mainloop()
