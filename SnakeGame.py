d82f9376978c6ced5899261ccea15139364c66df
Robin1331 <robin.bilo13@gmail.com>
2024-09-10 21:08:38 -0400

    Added constants and classes needed for the game
    I added GAME_WIDTH and GAME_HEIGHT to measure the width and height of the game. SPEED determines the quickness of the game. SPACE_SIZE determines how large the items like the food and snake are in the game. BODY_PARTS is the amount of body parts that the snake starts with. SNAKE_COLOR, FOOD_COLOR, and BACKROUND_COLOR are pretty self explanatory.

--- SnakeGame.py
+++ SnakeGame.py
@@ -1,6 +1,17 @@
 from tkinter import *
 import random
 
+GAME_WIDTH = 800
+GAME_HEIGHT = 800
+SPEED = 50
+SPACE_SIZE = 45
+BODY_PARTS = 3
+SNAKE_COLOR = "#4eba6b"
+FOOD_COLOR = "#ff0000"
+BACKROUND_COLOR = "#000000"
+
+
+
 class Snake:
     pass
 
@@ -10,7 +21,7 @@ class Food:
 def next_turn():
     pass
 
-def change_direction():
+def change_direction(new_direction):
     pass
 
 def check_collisions():
