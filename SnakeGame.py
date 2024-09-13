29e9972f1471fddb2c61b8b9855ec8bd9416a7aa
Robin1331 <robin.bilo13@gmail.com>
2024-09-13 11:20:49 -0400

    Created the snake shape, colour, and size

--- SnakeGame.py
+++ SnakeGame.py
@@ -21,7 +21,11 @@ class Snake:
 
         for i in range(0, BODY_PARTS):
             self.coordinates.append([0, 0])
+
+        for x, y in self.coordinates:
+            square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR, tag="snake")
             
+
 
 
 
